4e3b7bf14c9c5339862efe32a9853d7ab62f0904
Cryptodai3 <cryptodai3@gmail.com>
2025-06-30 12:48:36 +0545

    V1.0

--- bot.py
+++ bot.py
@@ -489,7 +489,7 @@ class Goblin:
                                 f"{Fore.CYAN + Style.BRIGHT} Status  : {Style.RESET_ALL}"
                                 f"{Fore.RED + Style.BRIGHT}Not Complete{Style.RESET_ALL}"
                             )
-                            continue
+                        continue
 
                     self.log(
                         f"{Fore.MAGENTA + Style.BRIGHT}     >{Style.RESET_ALL}"
